ecd1f4f745c767b0509f74d8ca668c242579ddb7
Home <ildar86@live.ru>
2022-12-08 20:36:07 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("       ООП. Урок 3. Наследование");
+        System.out.println();
+        System.out.println("    Черновик");
+
+    }
+}
